df5a8342317a77565e3651e52650fb413c808f35
tianshaoait <31428233+tianshaoait@users.noreply.github.com>
2017-08-31 11:31:54 +0800

    two
    two

--- app/src/main/java/com/tianshaoait/administrator/intypelayoutdome/MainActivity.java
+++ app/src/main/java/com/tianshaoait/administrator/intypelayoutdome/MainActivity.java
@@ -2,23 +2,52 @@ package com.tianshaoait.administrator.intypelayoutdome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.tianshaoait.administrator.mylibrary.InTypeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     InTypeLayout inTypeLayout;
+    Button button1,button2;
+    String aaaaaaaa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inTypeLayout = (InTypeLayout) findViewById(R.id.main_intypelayout);
-        String a = "";
-        if (a == null) {
+        button1 = (Button) findViewById(R.id.main_btn1);
+        button2 = (Button) findViewById(R.id.main_btn2);
+        if (aaaaaaaa == null) {
             inTypeLayout.show("数据错误", R.mipmap.ic_launcher);
         } else {
             inTypeLayout.clear();
         }
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aaaaaaaa = null;
+                if (aaaaaaaa == null) {
+                    inTypeLayout.show("数据错误", R.mipmap.ic_launcher);
+                } else {
+                    inTypeLayout.clear();
+                }
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aaaaaaaa = "1";
+                if (aaaaaaaa == null) {
+                    inTypeLayout.show("数据错误", R.mipmap.ic_launcher);
+                } else {
+                    inTypeLayout.clear();
+                }
+            }
+        });
+
+
     }
 }
--- mylibrary/src/main/java/com/tianshaoait/administrator/mylibrary/InTypeLayout.java
+++ mylibrary/src/main/java/com/tianshaoait/administrator/mylibrary/InTypeLayout.java
@@ -1,6 +1,8 @@
 package com.tianshaoait.administrator.mylibrary;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +16,12 @@ import android.widget.TextView;
 
 public class InTypeLayout extends LinearLayout {
 
+    private static final int DEFAULT_TEXTSIZE = 16;
 
     private View content;
+    private ColorStateList mTextColor;
+    private int mTextSize = DEFAULT_TEXTSIZE;
+    private int inTypeImageHeightWidth = 1;
 
     LinearLayout in_ly;
     ImageView in_img;
@@ -23,10 +29,35 @@ public class InTypeLayout extends LinearLayout {
 
     public InTypeLayout(Context context) {
         super(context, null);
+        init(context, null, 0);
     }
 
     public InTypeLayout(Context context, AttributeSet attrs) {
-        super(context, attrs, 0);
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
+
+    public InTypeLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs, defStyle);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        initAttrs(context, attrs, defStyleAttr);
+    }
+
+    private void initAttrs(Context context, AttributeSet attrs, int defStyleAttr) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.InTypeLayout, defStyleAttr, 0);
+        mTextColor = a.getColorStateList(R.styleable.InTypeLayout_inTypeTextColor);
+        if (mTextColor == null) {
+            mTextColor = ColorStateList.valueOf(getResources().getColor(android.R.color.primary_text_light));
+        }
+        int textSize = a.getDimensionPixelOffset(R.styleable.InTypeLayout_inTypeTextSize, 15);
+        if (textSize != -1) {
+            this.mTextSize = Util.px2sp(context, textSize);
+        }
+        inTypeImageHeightWidth = a.getInt(R.styleable.InTypeLayout_inTypeImageHeightWidth, 1);
+        a.recycle();
     }
 
     @Override
@@ -40,6 +71,15 @@ public class InTypeLayout extends LinearLayout {
         in_ly = (LinearLayout) findViewById(R.id.in_ly);
         in_img = (ImageView) findViewById(R.id.in_img);
         in_tx = (TextView) findViewById(R.id.in_tx);
+        if (mTextColor != null){
+            in_tx.setTextColor(mTextColor);
+        }
+        in_tx.setTextSize(mTextSize);
+        if(inTypeImageHeightWidth != 1){
+            LinearLayout.LayoutParams params= (LinearLayout.LayoutParams) in_img.getLayoutParams();
+            params.height = inTypeImageHeightWidth;
+            in_img.setLayoutParams(params);
+        }
     }
 
     public void show(String message, int drawable) {
